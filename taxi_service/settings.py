b5ed9ff659e94f388facf5924ec7759295ed60b3
Marko Khodan <khodanmarko@gmail.com>
2022-08-04 19:02:22 +0300

    settings and requirements updated

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -24,9 +24,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["taxi-sevice-app.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "taxi-sevice-app.herokuapp.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
